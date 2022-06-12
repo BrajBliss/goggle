98e88f6db2d4bc717a2afb99a9b459af498e982f
Amay <brajbliss@gmail.com>
2022-06-12 16:03:10 +0530

    credit hehe

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,11 @@ export const Footer = () => {
 	return (
 		<div className='text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-300'>
 			<h1>2022 Goggle, Inc.</h1>
+			<p>
+				<a href='https://amay.bio.link'>
+					Contact @BrajBliss! 👋
+				</a>
+			</p>
 		</div>
 	);
 };
