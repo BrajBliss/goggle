72775d410cc71a747489a8734c70ed953c84b8cd
Amay <brajbliss@gmail.com>
2022-06-12 16:07:33 +0530

    credit alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,12 @@ export const Footer = () => {
 	return (
 		<div className='text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-300'>
 			<h1>2022 Goggle, Inc.</h1>
-			<p>
-				<a href='https://amay.bio.link'>
+			<p className='mt-2'>
+				<a
+					href='https://amay.bio.link'
+					target='_blank'
+					rel='noreferrer'
+					className=' bg-blue-500 text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900'>
 					Contact @BrajBliss! 👋
 				</a>
 			</p>
