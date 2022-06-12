e21a5e9ef583f52b3ee6c6537773744f2ff262b0
Amay <brajbliss@gmail.com>
2022-06-12 11:30:22 +0530

    roboto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Search } from './Search';
 
 export const Navbar = ({ darkTheme, setDarkTheme }) => {
 	return (
@@ -17,6 +18,7 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
 					{darkTheme ? 'Light 💡' : 'Dark 🌙'}
 				</button>
 			</div>
+			<Search />
 		</div>
 	);
 };
