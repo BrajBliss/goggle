793ec8b598522e6c1f4d3a51ff02d59cf8627b39
Amay <brajbliss@gmail.com>
2022-06-12 15:33:23 +0530

    minor change ugh

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ import { useResultContext } from '../contexts/ResultContextProvider.js';
 import { Links } from './Links';
 
 export const Search = () => {
-	const [text, setText] = useState('Elon Musk');
+	const [text, setText] = useState('');
 	const { setSearchTerm } = useResultContext();
 	const [debouncedValue] = useDebounce(text, 300);
 
